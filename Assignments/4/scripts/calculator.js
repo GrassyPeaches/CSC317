c779798c66e1b95a9d81e8b3e0d878af8696ca1b
Grassy_Peaches <gracirami97@gmail.com>
2025-11-12 17:16:27 -0800

    Write key event listeners for JS.

---
+++ Assignments/4/scripts/calculator.js
@@ -0,0 +1,41 @@
+// listen to all key[ressed and determine the type pf key being pressed
+
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculator__keys");
+
+keys.addEventListener("click", (e) => {
+  if (e.target.matches("button")) {
+    const key = e.target;
+    const action = key.dataset.action;
+    
+    if (!action) {
+      console.log("number key!");
+    }
+
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      console.log("operator key!");
+    }
+
+    if (action === "decimal") {
+      console.log("decimal key!");
+    }
+
+    if (action === "clear") {
+      console.log("clear key!");
+    }
+
+    if (action === "calculate") {
+      console.log("equal key!");
+    }
+
+  }
+});
+
+
+
+
